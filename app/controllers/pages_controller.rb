811a1c36e9ae496242ba6405beb2a5b1d0770d34
beranemo <beranemo@gmail.com>
2017-12-25 02:28:35 +0800

    render plain

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,9 @@ class PagesController < ApplicationController
     render(html: "<h2>你好，世界</h2>".html_safe)
   end
   
+  # 注意：這樣只是純文本，不會吃到 html 標籤
+  def hello2
+    render(plain: "<p>你好，世界</p>")
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   get "/hello_world", to: "pages#hello"
   
+  get "/hello_world_2", to: "pages#hello2"
+  
   resources :posts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
